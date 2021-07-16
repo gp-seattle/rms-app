bfd8d60097df883da6497d4349af1db2bbdee4c2
Russell500 <ron500@uw.edu>
2021-07-16 15:54:46 -0700

    GH-29: Implement BorrowItem Lambda Handler

--- amplify/backend/ts-code/__tests__/api/BorrowItem.test.ts
+++ amplify/backend/ts-code/__tests__/api/BorrowItem.test.ts
@@ -1,6 +1,7 @@
 import { BorrowItem } from "../../src/api/BorrowItem"
 import { DBSeed, TestConstants, TestTimestamps } from "../../__dev__/db/DBTestConstants"
 import { LocalDBClient } from "../../__dev__/db/LocalDBClient"
+import {BorrowBatch} from "../../src/api/BorrowBatch";
 
 test('will borrow item correctly when id exists', async () => {
     const dbClient: LocalDBClient = new LocalDBClient(DBSeed.ONE_NAME)
@@ -50,5 +51,36 @@ test('will fail to borrow item when id does not exist', async () => {
             notes: TestConstants.NOTES
         }).then(() => dbClient.getDB())
     ).rejects.toThrow(`Couldn't find item ${TestConstants.BAD_REQUEST} in the database.`)
+    expect(dbClient.getDB()).toEqual(DBSeed.ONE_NAME)
+})
+test('will fail to borrow item when item name not passed in', async () => {
+    const dbClient: LocalDBClient = new LocalDBClient(DBSeed.ONE_NAME)
+    const api: BorrowItem = new BorrowItem(dbClient)
+
+    // Mock Date
+    Date.now = jest.fn(() => TestTimestamps.BORROW_ITEM)
+
+    await expect(
+        api.execute({
+            borrower: TestConstants.BORROWER,
+            notes: TestConstants.NOTES
+        })
+    ).rejects.toThrow("Missing required field 'ids'")
+    expect(dbClient.getDB()).toEqual(DBSeed.ONE_NAME)
+})
+
+test('will fail to borrow item when item borrower not passed in', async () => {
+    const dbClient: LocalDBClient = new LocalDBClient(DBSeed.ONE_NAME)
+    const api: BorrowItem = new BorrowItem(dbClient)
+
+    // Mock Date
+    Date.now = jest.fn(() => TestTimestamps.BORROW_ITEM)
+
+    await expect(
+        api.execute({
+            ids: [ TestConstants.ITEM_ID ],
+            notes: TestConstants.NOTES
+        })
+    ).rejects.toThrow("Missing required field 'borrower'")
     expect(dbClient.getDB()).toEqual(DBSeed.ONE_NAME)
 })
