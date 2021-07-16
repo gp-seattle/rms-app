d0cbb56266149b72abfd4c5c94f7e70b6f838aea
Daniel Barnes <dbarnes2@cs.washington.edu>
2021-07-15 19:55:28 -0700

    GH-57: Change Background Color to Black

--- App.js
+++ App.js
@@ -14,7 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ccc',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
   },
