fcb59703a0b6f63e6d46f120b3550e052169e34d
Russell500 <ron500@uw.edu>
2021-07-28 10:06:56 -0700

    GH-32: Implement DeleteItem Lambda Handler

---
+++ amplify/backend/ts-code/__tests__/handlers/api/DeleteItem.test.ts
@@ -0,0 +1,21 @@
+import getClients from "../../../src/handlers/api/APIHelper"
+import { handler } from "../../../src/handlers/api/DeleteItem"
+import { DBSeed, TestConstants} from "../../../__dev__/db/DBTestConstants"
+import { LocalDBClient } from "../../../__dev__/db/LocalDBClient"
+import { LocalMetricsClient } from "../../../__dev__/metrics/LocalMetricsClient"
+
+test('will delete item correctly when using handler', async () => {
+    const dbClient: LocalDBClient = new LocalDBClient(DBSeed.TWO_NAMES)
+    const metricsClient: LocalMetricsClient = new LocalMetricsClient()
+
+    getClients.getDBClient = jest.fn(() => dbClient)
+    getClients.getMetricsClient = jest.fn(() => metricsClient)
+
+    await expect(
+        handler({
+            id: TestConstants.ITEM_ID_2
+        }, null, null)
+    ).resolves.toEqual(`Deleted a '${TestConstants.NAME_2}' from the inventory.`)
+    expect(dbClient.getDB()).toEqual(DBSeed.ONE_NAME)
+    metricsClient.assureState(0)
+})
