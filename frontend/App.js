cd85bc841db20890a43b1893fefcbfce7c6f937a
navigsb <navigsb@uw.edu>
2021-08-02 16:53:52 -0700

    GH-107: Setting up Redux and Creating API For Easy Storage

--- frontend/App.js
+++ frontend/App.js
@@ -2,13 +2,17 @@ import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { registerRootComponent } from "expo";
+import { Provider } from "react-redux";
+import store from "./store/store";
 
 function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Provider store={store}>
+      <View style={styles.container}>
+        <Text>I am an app now powered by Redux!</Text>
+        <StatusBar style="auto" />
+      </View>
+    </Provider>
   );
 }
 
---
+++ frontend/store/store.js
@@ -0,0 +1,9 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { getSlicesConfig } from "./sliceManager";
+import counterSlice from "./slices/counterSlice";
+
+export default configureStore(
+  getSlicesConfig({
+
+  })
+);
