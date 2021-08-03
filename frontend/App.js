e31ee51e8c3ca5fbf448f349596afb78465e0b44
James Lee <jamestlee513@gmail.com>
2021-08-02 17:30:17 -0700

    GH-117: Structure frontend directory and create MockComponent

--- frontend/App.js
+++ frontend/App.js
@@ -1,8 +1,9 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { registerRootComponent } from "expo";
 
-export default function App() {
+function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
@@ -19,3 +20,5 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
 });
+
+export default registerRootComponent(App);
