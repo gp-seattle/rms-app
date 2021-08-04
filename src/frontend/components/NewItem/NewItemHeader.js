32ad06b84dc8c5076091dfb097a82e051b355b50
Runsen Wu <merkurieffr@gmail.com>
2021-08-04 12:10:45 -0700

    adding styling to the header of the application

--- src/frontend/components/NewItem/NewItemHeader.js
+++ src/frontend/components/NewItem/NewItemHeader.js
@@ -1,20 +1,50 @@
-import React from "react";
-import { Button } from "react-native";
-import { StyleSheet, Text, View } from "react-native";
+import React from 'react';
+import { Button } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 const NewItemHeader = ({ onBackPress, title, onCancelPress }) => {
-  return (
-    <View
-      style={{
-        borderWidth: 2,
-        borderColor: "black",
-      }}
-    >
-      <Text onPress={onBackPress}>Here would go a chevron to the left</Text>
-      <Text>Here would go a little title</Text>
-      <Button title="I'm a button" onPress={onCancelPress} />
-    </View>
-  );
+	return (
+		<View style={styles.headerContainer}>
+			<View style={styles.newItem}>
+				<Text onPress={onBackPress} style={styles.backArrow}>
+					{' <'}{' '}
+				</Text>
+				<Text style={styles.text}>New Item</Text>
+			</View>
+			<View style={styles.button}>
+				<Button title="cancel" onPress={onCancelPress} />
+			</View>
+		</View>
+	);
 };
+
+const styles = StyleSheet.create({
+	headerContainer: {
+		borderWidth: 2,
+		borderColor: 'black',
+		alignItems: 'center',
+		flexDirection: 'row',
+		height: 150,
+		justifyContent: 'space-between',
+	},
+	newItem: {
+		flexDirection: 'row',
+	},
+	backArrow: {
+		borderColor: 'black',
+		borderWidth: 1,
+		justifyContent: 'flex-start',
+		width: 20,
+	},
+	text: {
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	button: {
+		borderColor: 'black',
+		borderWidth: 1,
+		justifyContent: 'flex-end',
+	},
+});
 
 export default NewItemHeader;
--- src/frontend/components/NewItem/index.js
+++ src/frontend/components/NewItem/index.js
@@ -1,16 +1,28 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 import NewItemForm from './NewItemForm';
 import NewItemHeader from './NewItemHeader';
 
 const NewItem = () => {
-    return (
-        <View>
-            <NewItemHeader />
-            <NewItemForm />
-        </View>
+	return (
+		<View >
+			<NewItemHeader />
+			<NewItemForm />
+		</View>
+	);
+};
 
-    );
-}
+const styles = StyleSheet.create({
+	headerContainer: {
+		justifyContent: 'flex-start',
+		alignItems: 'flex-start',
+	},
+	input: {
+		width: '80%',
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 10,
+	},
+});
 
-export default NewItem
+export default NewItem;
