a837947577ebccea2c9a31df942e1cecde89b09b
Johnny Chen <49621623+johnnyhchen@users.noreply.github.com>
2021-08-05 10:01:35 -0700

    GH-54: Create a new GraphQL schema
    GH-131: Fix ItemTable key
    
    GH-133: Update key for items table
    
    GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Dim background attempt
    
    * GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Added Action Dialog
    
    * GH-127: Fixing errors
    
    * GH-127: Revisions
    
    GH-115: Create method in AddItem to auto-generate the ID (#142)
    
    GH-121: Function Integration Tests (#141)
    
    * GH-121: Functions Integration Tests
    
    GH-143: Change History item id to be called itemId
    
    * GH-143: Change History item id to be called itemId
    
    * Update seeds to let tests pass
    
    GH-20: Move ts-code outside of amplify/backend directory
    
    - Because that directory gets overidden on git pull
    - Also Integration test after amplify push
    
    GH-116: Auto-Supply ID during AddItem
    
    Created functional tab navigation system

---
+++ amplify/ts-code/__tests__/integration/Amplify.test.ts
@@ -0,0 +1,115 @@
+import { ICredentials } from '@aws-amplify/core'
+import { Amplify, Auth } from 'aws-amplify'
+import { InvocationResponse } from 'aws-sdk/clients/lambda'
+import { TestConstants } from "../../__dev__/db/DBTestConstants"
+import AWS = require('aws-sdk')
+import Lambda = require( 'aws-sdk/clients/lambda')
+const AWSConfig = require('../../../../src/aws-exports').default
+
+const ENV_SUFFIX = '-alpha'
+const ENV_REGION = 'us-west-2'
+
+describe('Amplify Tests', () => {
+    beforeAll(() => {
+        Amplify.configure(AWSConfig)
+    })
+
+    /**
+     * TODO: Single Use. Left here for validation purposes.
+     *
+    test('will sign up when api is called', async () => {
+        await expect(
+            Auth.signUp({
+            username: TestConstants.EMAIL,
+            password: TestConstants.PASSWORD,
+            attributes: {
+                email: TestConstants.EMAIL,
+                name: TestConstants.OWNER
+            }
+        ).resolves
+    })
+    */
+
+    /**
+     * AUTH: Sign In
+     */
+    test('will sign in when api is called', async () => {
+        await expect(
+            Auth.signIn({
+                username: TestConstants.EMAIL,
+                password: TestConstants.PASSWORD
+            }).then(() => Auth.currentCredentials())
+            .then((credentials: ICredentials) => credentials.authenticated)
+        ).resolves.toEqual(true)
+    })
+
+    /**
+     * add item using api
+    */
+    test('will add item when api is called', async() => {
+            await expect(
+                Auth.currentCredentials()
+                    .then((credentials:ICredentials) => {
+                        AWS.config.credentials = credentials
+                        const lambda = new Lambda({
+                            credentials: credentials,
+                            region: ENV_REGION
+                        })
+                        return lambda.invoke({
+                            FunctionName: `AddItem${ENV_SUFFIX}`,
+                            Payload: JSON.stringify({
+                                id: TestConstants.ITEM_ID,
+                                name: TestConstants.DISPLAYNAME,
+                                description: TestConstants.DESCRIPTION,
+                                tags: [TestConstants.TAG],
+                                owner: TestConstants.OWNER,
+                                notes: TestConstants.NOTES
+                            })
+                        }).promise()
+                    }).then((response: InvocationResponse) => {
+                        return response.Payload
+                    })
+            ).resolves.toEqual(`"Created Item with RMS ID: ${TestConstants.ITEM_ID}"`)
+    })
+
+
+    /**
+     * delete item using api
+    */
+    test('will delete item when api is called', async() => {
+        await expect(
+            Auth.currentCredentials()
+                .then((credentials:ICredentials) => {
+                    AWS.config.credentials = credentials
+                    const lambda = new Lambda({
+                        credentials: credentials,
+                        region: ENV_REGION
+                    })
+                    return lambda.invoke({
+                        FunctionName: `DeleteItem${ENV_SUFFIX}`,
+                        Payload: JSON.stringify({
+                            id: TestConstants.ITEM_ID,
+                            name: TestConstants.DISPLAYNAME,
+                            description: TestConstants.DESCRIPTION,
+                            tags: [TestConstants.TAG],
+                            owner: TestConstants.OWNER,
+                            notes: TestConstants.NOTES
+                        })
+                    }).promise()
+                }).then((response: InvocationResponse) => {
+                    return response.Payload
+                })
+        ).resolves.toEqual(`"Deleted a '${TestConstants.NAME}' from the inventory."`)
+    })
+
+    /**
+     * AUTH: Sign Out
+     */
+    test('will sign out when api is called', async () => {
+        await expect(
+            Auth.signOut()
+            .then(() => Auth.currentCredentials())
+            .then((exception: any) => exception.name)
+        ).resolves.toEqual("NotAuthorizedException")
+    })
+});
---
+++ amplify/ts-code/__tests__/unit/handlers/api/ReturnItem.test.ts
@@ -0,0 +1,26 @@
+import getClients from "../../../../src/handlers/api/APIHelper"
+import { handler } from "../../../../src/handlers/api/ReturnItem"
+import { DBSeed, TestConstants, TestTimestamps } from "../../../../__dev__/db/DBTestConstants"
+import { LocalDBClient } from "../../../../__dev__/db/LocalDBClient"
+import { LocalMetricsClient } from "../../../../__dev__/metrics/LocalMetricsClient"
+
+test('will return item correctly when using handler', async () => {
+    const dbClient: LocalDBClient = new LocalDBClient(DBSeed.ONE_NAME_BORROWED)
+    const metricsClient: LocalMetricsClient = new LocalMetricsClient()
+
+    getClients.getDBClient = jest.fn(() => dbClient)
+    getClients.getMetricsClient = jest.fn(() => metricsClient)
+    
+    // Mock Date
+    Date.now = jest.fn(() => TestTimestamps.RETURN_ITEM)
+    
+    await expect(
+        handler({
+            ids: [ TestConstants.ITEM_ID ],
+            borrower: TestConstants.BORROWER,
+            notes: TestConstants.NOTES_2
+        }, null, null)
+    ).resolves.toEqual(`Successfully returned items '${TestConstants.ITEM_ID}'.`)
+    expect(dbClient.getDB()).toEqual(DBSeed.ONE_NAME_RETURNED)
+    metricsClient.assureState(0)
+})
