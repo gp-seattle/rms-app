3df1620b89bf59b765b81f8562750c68a2ebdf76
Jeremy Yau <jeremyyau@me.com>
2021-08-09 09:08:05 -0700

    Create Seperate Unit and Integ Test Scripts

---
+++ backend-integ-test.js
@@ -0,0 +1,41 @@
+/**
+ * Test Backend Code
+ */
+const { exec } = require("child_process")
+
+const MASTER_PATH = __dirname
+
+const STORAGE_MAIN_NAME="main"
+const STORAGE_ITEMS_NAME="items"
+const STORAGE_BATCH_NAME="batch"
+const STORAGE_TAGS_NAME="tags"
+const STORAGE_HISTORY_NAME="history"
+const STORAGE_TRANSACTIONS_NAME="transactions"
+
+// Compile Typescript
+exec("jest --json --outputFile=report.json --testPathPattern='.*/integration/.*.test.js'",
+    {
+        cwd: MASTER_PATH,
+        env: {
+            ...process.env,
+            STORAGE_MAIN_NAME,
+            STORAGE_ITEMS_NAME,
+            STORAGE_BATCH_NAME,
+            STORAGE_TAGS_NAME,
+            STORAGE_HISTORY_NAME,
+            STORAGE_TRANSACTIONS_NAME
+        }
+    },
+    (error, stdout, stderr) => {
+        if (stderr) {
+            console.error(`stderr: ${stderr}`);
+        }
+        if (stdout) {
+            console.log(`stdout: ${stdout}`);
+        }
+        if (error) {
+            console.error(`exec error: ${error}`);
+            return;
+        }
+    }
+)
