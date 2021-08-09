58e341874431a5df69b8f03d794018bdaa5c8ae7
Jeremy Yau <jeremyyau@me.com>
2021-08-09 09:22:10 -0700

    Update Integ Test to not collect coverage

--- backend-integ-test.js
+++ backend-integ-test.js
@@ -5,26 +5,10 @@ const { exec } = require("child_process")
 
 const MASTER_PATH = __dirname
 
-const STORAGE_MAIN_NAME="main"
-const STORAGE_ITEMS_NAME="items"
-const STORAGE_BATCH_NAME="batch"
-const STORAGE_TAGS_NAME="tags"
-const STORAGE_HISTORY_NAME="history"
-const STORAGE_TRANSACTIONS_NAME="transactions"
-
 // Compile Typescript
-exec("jest --json --outputFile=report.json --testPathPattern='.*/integration/.*.test.js'",
+exec("jest --coverage=false --testPathPattern='.*/integration/.*.test.js'",
     {
-        cwd: MASTER_PATH,
-        env: {
-            ...process.env,
-            STORAGE_MAIN_NAME,
-            STORAGE_ITEMS_NAME,
-            STORAGE_BATCH_NAME,
-            STORAGE_TAGS_NAME,
-            STORAGE_HISTORY_NAME,
-            STORAGE_TRANSACTIONS_NAME
-        }
+        cwd: MASTER_PATH
     },
     (error, stdout, stderr) => {
         if (stderr) {
