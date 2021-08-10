46bdf0717985930fa7eaa03c2cdcf8d75a5365ec
Annie0728 <annie.xu777@yahoo.com>
2021-08-10 15:18:17 -0700

    GH-109: Attempting Write API

--- frontend/components/Util/UtilWrite.js
+++ frontend/components/Util/UtilWrite.js
@@ -1,94 +1,49 @@
-import { Amplify, Auth } from 'aws-amplify';
-import AWS from 'aws-sdk';
-import * as Linking from 'expo-linking';
-import * as WebBrowser from 'expo-web-browser';
-import 'react-native-get-random-values';
-import 'react-native-url-polyfill/auto';
-import { TestConstants } from '../../../amplify/ts-code/__dev__/db/DBTestConstants';
-import awsconfig from '../../../src/aws-exports';
+const ENV_SUFFIX = '-alpha'
+const ENV_REGION = 'us-west-2'
 
-const ENV_SUFFIX = '-alpha';
-const ENV_REGION = 'us-west-2';
+function UtilWrite(props) {
+    const lambda = new AWS.Lambda({
+        acessKeyId: 'AKIA3VBTB7LD2WMDBRDH',
+        secretAccessKey: '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',
+        region: ENV_REGION
+    })
 
-async function urlOpener(url, redirectUrl) {
-	const { type, url: newUrl } = await WebBrowser.openAuthSessionAsync(url, redirectUrl);
+    var params = {
+        FunctionName: `AddItem${ENV_SUFFIX}`,
+        Payload: JSON.stringify({
+            id: 2,
+            name: "test front-end",
+            description: "pls work",
+            tags: ["test1", "test2"],
+            owner: "annie",
+            notes: "test notes"
+        })
+    };
 
-	if (type === 'success' && Platform.OS === 'ios') {
-		WebBrowser.dismissBrowser();
-		return Linking.openURL(newUrl);
-	}
-}
+    function AddItem() {
+        return lambda.invoke(params,function (err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            }
+            else {
+                console.log(data);
+            }
+        });
+    }
 
-export async function AddNewItem(name, description, location, amount, categories) {
-	const notes = JSON.stringify({
-		capsName: name,
-		description,
-		location,
-		amount,
-		categories,
-	});
-	let resultId = await invoke({
-		FunctionName: `AddItem${ENV_SUFFIX}`,
-		Payload: JSON.stringify({
-			name,
-			tags: categories,
-			notes,
-		}),
-	}).Payload;
-	if (resultId) {
-		return resultId.substring(1, resultId.length - 1);
-	} else {
-		return undefined;
-	}
-}
+    AddItem();
 
-export async function DeleteItem(id) {
-	await invoke({
-		FunctionName: `DeleteItem${ENV_SUFFIX}`,
-		Payload: JSON.stringify({
-			id,
-		}),
-	});
-}
-
-export async function AmplifyInit() {
-	Amplify.configure({
-		...awsconfig,
-		oauth: {
-			...awsconfig.oauth,
-			urlOpener,
-		},
-	});
-	await Auth.signIn({
-		username: TestConstants.EMAIL,
-		password: TestConstants.PASSWORD,
-	});
-	return await Auth.currentCredentials();
-}
-
-async function close() {
-	await Auth.signOut();
-	try {
-		await Auth.currentCredentials();
-	} catch (e) {
-		console.error(e);
-	}
-}
-
-async function invoke(params) {
-	const credentials = await Auth.currentCredentials();
-	AWS.config.credentials = credentials;
-	const lambda = new AWS.Lambda({
-		credentials: credentials,
-		region: ENV_REGION,
-	});
-	let resultData;
-	lambda.invoke(params, function (data, err) {
-		if (err) {
-			console.log(err, err.stack);
-		} else {
-			resultData = data;
-		}
-	});
-	return resultData;
+    function DeleteItem() {
+        lambda.invoke({
+            FunctionName: `DeleteItem${ENV_SUFFIX}`,
+            Payload: JSON.stringify({
+                id: props.id,
+                name: props.name,
+                description: props.description,
+                tags: [props.tags],
+                owner: props.owner,
+                notes: props.notes
+            })
+        });
+    }
 }
