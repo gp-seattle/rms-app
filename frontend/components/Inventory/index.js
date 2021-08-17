8ceb2c39bcd4f89fa6dcdfd02aade4fc70400684
Runsen Wu <merkurieffr@gmail.com>
2021-08-17 11:42:09 -0700

    updating the inventory page

--- frontend/components/Inventory/index.js
+++ frontend/components/Inventory/index.js
@@ -1,50 +1,76 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { useReduxSliceProperty } from '../../store/sliceManager';
-import itemsSlice from '../../store/slices/itemsSlice';
-import ListElement from '../ListElement';
 import { Searchbar } from 'react-native-paper';
-import { ScrollView } from 'react-native-gesture-handler';
+import SubmitBar from '../SubmitBar';
+import InventoryScrollView from './InventoryScrollView';
 
-const Inventory = () => {
-	const itemsState = useReduxSliceProperty(itemsSlice);
+const Inventory = (props) => {
 	const [searchQuery, setSearchQuery] = useState('');
-
 	const onChangeSearch = (query) => setSearchQuery(query);
 
+	// using filter array for the search bar
+
 	return (
-		<View>
-			<ScrollView ScrollView style={styles.container} contentContainerStyle={styles.container}>
-				<Searchbar
-					placeholder="Search"
-					onChangeText={onChangeSearch}
-					value={searchQuery}
-					style={{ marginTop: 50 }}
-				/>
-				<View style={{ marginTop: 50, marginLeft: '5%' }}>
-					{itemsState.items.map((item) => (
-						<ListElement
-							key={item.id}
-							iconLeft={item.iconName}
-							height={50}
-							iconColor="black"
-							iconSize={25}>
-							<Text>{item.name}</Text>
-						</ListElement>
-					))}
+			<View style={{ backgroundColor: '#FFF', paddingLeft: '3%', paddingRight: '3%' , height: '100%'}}>
+				<View style={styles.textAndSearch}>
+					<Text style={styles.inventoryText}>Inventory</Text>
+					<Searchbar
+						style={styles.searchBar}
+						placeholder="Search"
+						onChangeText={onChangeSearch}
+						value={searchQuery}
+					/>
 				</View>
-			</ScrollView>
-		</View>
+				<InventoryScrollView />
+				<SubmitBar
+					iconName="plus"
+					submitText="Add Item"
+					submitDisabled={false}
+					style={{bottom: '4%'}}
+					onSubmit={props.onAddItem}
+				/>
+			</View>
 	);
 };
 
+{
+	/* <View>
+	<View style={{ backgroundColor: '#FFF', paddingLeft: '3%', paddingRight: '3%' }}>
+		<View style={styles.textAndSearch}>
+			<Text style={styles.inventoryText}>Inventory</Text>
+			<Searchbar
+				style={styles.searchBar}
+				placeholder="Search"
+				onChangeText={onChangeSearch}
+				value={searchQuery}
+			/>
+		</View>
+
+		<InventoryScrollView />
+	</View>
+</View>; */
+}
+
 const styles = StyleSheet.create({
-	container: {
+	textAndSearch: {
 		backgroundColor: '#FFF',
 		paddingLeft: '3%',
 		paddingRight: '3%',
+		width: '100%',
+		justifyContent: 'flex-start',
+		marginTop: '20%',
+	},
+	inventoryText: {
+		fontSize: 35,
+		fontWeight: 'bold',
+		marginBottom: '10%',
+	},
+	searchBar: { borderWidth: 1, elevation: 0, shadowOpacity: 0 },
+	container: {
+		paddingLeft: '1%',
+		paddingRight: '3%',
 		height: '100%',
-	}
-})
+	},
+});
 
 export default Inventory;
