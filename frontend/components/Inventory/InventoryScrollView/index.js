1d7e4979a591c2377fa2eb3eb4772949448ecc35
Runsen Wu <merkurieffr@gmail.com>
2021-08-19 14:25:06 -0700

    GH-189/wu-chiense-new fixing final style for the application

--- frontend/components/Inventory/InventoryScrollView/index.js
+++ frontend/components/Inventory/InventoryScrollView/index.js
@@ -1,116 +1,106 @@
 import React, { useState } from 'react';
 import { Text, View, StyleSheet } from 'react-native';
-import { useReduxSliceProperty } from '../../../store/sliceManager'
+import { useReduxSliceProperty } from '../../../store/sliceManager';
 import ListElement from '../../ListElement';
 import itemsSlice from '../../../store/slices/itemsSlice';
 import { ScrollView } from 'react-native-gesture-handler';
 import ListDropdown from '../../Dashboard/ListDropdown';
 import RMSIcon from '../../RMSIcon';
- 
-const InventoryScrollView = () => {
-   const itemsState = useReduxSliceProperty(itemsSlice);
-   const locations = [
-       {
-           label: 'All Items',
-           value: 'All Items',
-       },
-       {
-           label: 'Wedgewood',
-           value: 'Wedgwood',
-       },
-       {
-           label: '100s',
-           value: '100s',
-       },
-       {
-           label: "Yeh's",
-           value: "Yeh's",
-       },
-   ];
-   return (
-       <ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
-           <View style={{ marginLeft: '5%' }}>
-               <View marginTop="10%" justifyContent="space-around" height={200}>
-                   <ListElement paddingBottom={20} iconLeft="star" iconColor="black" iconSize={25}>
-                       <View flexDirection="row" justifyContent="space-between">
-                           <Text style={styles.textStyle}>Favorite</Text>
-                           <RMSIcon iconName="chevron-right" size={30} color="black" />
-                       </View>
-                   </ListElement>
- 
-                   <ListElement
-                       paddingBottom={20}
-                       iconLeft="format-list-bulleted"
-                       iconColor="black"
-                       iconSize={25}>
-                       <View flexDirection="row" justifyContent="space-between">
-                           <Text style={styles.textStyle}>Lists</Text>
-                           <RMSIcon iconName="chevron-right" size={30} color="black" />
-                       </View>
-                   </ListElement>
- 
-                   <ListElement
-                       paddingBottom={20}
-                       iconLeft="bookmark"
-                       iconColor="black"
-                       iconSize={25}>
-                       <View flexDirection="row" justifyContent="space-between">
-                           <Text style={styles.textStyle}>Inventory</Text>
-                           <RMSIcon iconName="chevron-right" size={30} color="black" />
-                       </View>
-                   </ListElement>
-               </View>
- 
-               <View style={styles.itemEditors}>
-                   <Text style={styles.itemText}>Items</Text>
-                   <ListDropdown list={locations} />
-               </View>
- 
-               {itemsState.items.map((item) => (
-                   <ListElement
-                       key={item.id}
-                       iconLeft={item.iconName}
-                       height={50}
-                       iconColor="black"
-                       iconSize={25}>
-                       <Text>{item.name}</Text>
-                   </ListElement>
-               ))}
-           </View>
-       </ScrollView>
-   );
-};
- 
-const styles = StyleSheet.create({
-   container: {
-       paddingLeft: '1%',
-       paddingRight: '3%',
-       height: '100%',
-   },
-   contentContainer: {
-       paddingLeft: '1%',
-       paddingRight: '3%',
-   },
-   itemText: {
-       fontSize: 27,
-       fontWeight: '500',
-   },
-   itemEditors: {
-       borderRadius: 12,
-       width: '100%',
-       paddingLeft: '3%',
-       paddingRight: '3%',
-       marginTop: '3%',
-       flexDirection: 'row',
-       justifyContent: 'space-between',
-       alignItems: 'center',
-       height: 70,
-   },
-   textStyle: {
-       fontSize: 23,
-   },
-});
- 
-export default InventoryScrollView;
- 
 
+const InventoryScrollView = () => {
+	const itemsState = useReduxSliceProperty(itemsSlice);
+	const locations = [
+		{
+			label: 'All Items',
+			value: 'All Items',
+		},
+		{
+			label: 'Wedgewood',
+			value: 'Wedgwood',
+		},
+		{
+			label: '100s',
+			value: '100s',
+		},
+		{
+			label: "Yeh's",
+			value: "Yeh's",
+		},
+	];
+	return (
+		<ScrollView style={styles.container} contentContainerStyle={styles.contentContainer}>
+			<View style={{ marginLeft: '5%' }}>
+				<View marginTop="10%" justifyContent="space-around" height={200}>
+					<ListElement iconLeft="star" iconColor="black" iconSize={25}>
+						<View flexDirection="row" justifyContent="space-between">
+							<Text style={styles.textStyle}>Favorite</Text>
+							<RMSIcon iconName="chevron-right" size={30} color="black" />
+						</View>
+					</ListElement>
+
+					<ListElement iconLeft="format-list-bulleted" iconColor="black" iconSize={25}>
+						<View flexDirection="row" justifyContent="space-between">
+							<Text style={styles.textStyle}>Lists</Text>
+							<RMSIcon iconName="chevron-right" size={30} color="black" />
+						</View>
+					</ListElement>
+
+					<ListElement iconLeft="bookmark" iconColor="black" iconSize={25}>
+						<View flexDirection="row" justifyContent="space-between">
+							<Text style={styles.textStyle}>Inventory</Text>
+							<RMSIcon iconName="chevron-right" size={30} color="black" />
+						</View>
+					</ListElement>
+				</View>
+
+				<View style={styles.itemEditors}>
+					<Text style={styles.itemText}>Items</Text>
+					<ListDropdown list={locations} />
+				</View>
+
+				{itemsState.items.map((item) => (
+					<ListElement
+						key={item.id}
+						iconLeft={item.iconName}
+						height={50}
+						iconColor="black"
+						iconSize={25}>
+						<Text>{item.name}</Text>
+					</ListElement>
+				))}
+			</View>
+		</ScrollView>
+	);
+};
+
+const styles = StyleSheet.create({
+	container: {
+		paddingLeft: '1%',
+		paddingRight: '3%',
+		height: '100%',
+	},
+	contentContainer: {
+		paddingLeft: '1%',
+		paddingRight: '3%',
+	},
+	itemText: {
+		fontSize: 27,
+		fontWeight: '500',
+	},
+	itemEditors: {
+		borderRadius: 12,
+		width: '100%',
+		paddingLeft: '3%',
+		paddingRight: '3%',
+		marginTop: '3%',
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center',
+		height: 70,
+	},
+	textStyle: {
+		fontSize: 23,
+	},
+});
+
+export default InventoryScrollView;
--- frontend/components/ListElement/index.js
+++ frontend/components/ListElement/index.js
@@ -1,63 +1,51 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 import RMSIcon from '../RMSIcon';
- 
-const ListElement = ({
-   paddingBottom,
-   height,
-   children,
-   iconRight,
-   iconLeft,
-   iconSize,
-   iconColor,
-}) => {
-   const styles = StyleSheet.create({
-       container: {
-           width: '100%',
-           flexDirection: 'row',
-           alignItems: 'center',
-       },
-       main: {
-           height: height ? height : '100%',
-           borderBottomWidth: 1,
-           borderColor: '#EEE',
-           justifyContent: 'center',
-           flexGrow: 1,
-           paddingBottom: paddingBottom ? paddingBottom : 0,
-       },
-       iconLeftStyle: {
-           marginRight: 30,
-           paddingBottom: paddingBottom ? paddingBottom : 0,
-       },
-   });
-   return (
-       <View style={styles.container}>
-           {iconLeft && (
-               <RMSIcon
-                   iconName={iconLeft}
-                   size={iconSize}
-                   color={iconColor}
-                   style={styles.iconLeftStyle}
-               />
-           )}
-           <View
-               style={{
-                   ...styles.main,
-               }}>
-               {children}
-           </View>
-           {iconRight && (
-               <RMSIcon
-                   iconName={iconRight}
-                   size={iconSize}
-                   color={iconColor}
-                   style={{ marginLeft: 30 }}
-               />
-           )}
-       </View>
-   );
-};
- 
-export default ListElement;
- 
 
+const ListElement = ({ height, children, iconRight, iconLeft, iconSize, iconColor }) => {
+	const styles = StyleSheet.create({
+		container: {
+			width: '100%',
+			flexDirection: 'row',
+			alignItems: 'center',
+		},
+		main: {
+			height: height ? height : '70%',
+			borderBottomWidth: 1,
+			borderColor: '#EEE',
+			justifyContent: 'center',
+			flexGrow: 1,
+		},
+		iconLeftStyle: {
+			marginRight: 30,
+		},
+	});
+	return (
+		<View style={styles.container}>
+			{iconLeft && (
+				<RMSIcon
+					iconName={iconLeft}
+					size={iconSize}
+					color={iconColor}
+					style={styles.iconLeftStyle}
+				/>
+			)}
+			<View
+				style={{
+					...styles.main,
+				}}>
+				{children}
+			</View>
+			{iconRight && (
+				<RMSIcon
+					iconName={iconRight}
+					size={iconSize}
+					color={iconColor}
+					style={{ marginLeft: 30 }}
+				/>
+			)}
+		</View>
+	);
+};
+
+export default ListElement;
